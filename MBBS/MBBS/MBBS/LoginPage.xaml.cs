7cc83be5b40939e65cef5b7f5581f76ab61caa6b
Carlo Ruhe <carlo_ruhe@msn.com>
2016-04-28 12:35:01 +0200

    LoginPage

---
+++ MBBS/MBBS/MBBS/LoginPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MBBS
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OnButtonSignUpClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
