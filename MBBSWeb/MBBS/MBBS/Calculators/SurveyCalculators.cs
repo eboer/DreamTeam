607a3aa071342960929e608de0ae6870ee093244
Janine <janine.lanting@gmail.com>
2016-05-25 23:05:01 +0200

    Survey queries and calculations

---
+++ MBBSWeb/MBBS/MBBS/Calculators/SurveyCalculators.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MBBS.Database;
+
+namespace MBBS.Calculators
+{
+    public class SurveyCalculators
+    {
+        public Dictionary<int, double> GetAverageRatingPerYear(string moduleID)
+        {
+            SurveyQueries query = new SurveyQueries();
+            Dictionary<int, double> results = new Dictionary<int, double>();
+            Dictionary<int, List<int>> resultList = query.GetAllSurveyResults(moduleID);
+            foreach(KeyValuePair<int, List<int>> year in resultList)
+            {
+                int yearTotal = 0;
+                int yearNumber = 0;
+                foreach(int rating in year.Value)
+                {
+                    if(rating != 0)
+                    {
+                        yearTotal += rating;
+                        yearNumber++;
+                    }
+                }
+                results.Add(year.Key, (double)yearTotal / (double)yearNumber);
+            }
+            return results;
+        }
+
+    }
+}
---
+++ MBBSWeb/MBBS/MBBS/Models/DocentModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MBBS.Models
+{
+    public class DocentModule
+    {
+        public string module_id { get; set; }
+        public string module_name { get; set; }
+    }
+}
