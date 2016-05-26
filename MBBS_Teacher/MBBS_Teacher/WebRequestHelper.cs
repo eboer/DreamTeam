44fde5dd620d11d31c1252cede1afb91f3dbb9ac
Ruben.de.witte <rubendewitte93@gmail.com>
2016-05-26 14:49:34 +0200

    update in the webRequestHelper

--- MBBS_Teacher/MBBS_Teacher/WebRequestHelper.cs
+++ MBBS_Teacher/MBBS_Teacher/WebRequestHelper.cs
@@ -10,12 +10,25 @@ namespace MBBS_Teacher
 {
    static class WebRequestHelper
     {
-        public static object getData(string url )
+        public static string getData(string url, string authorization)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", authorization);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream resStream = response.GetResponseStream();
-            return (object)resStream;
+            StreamReader reader = new StreamReader(resStream);
+            string text = reader.ReadToEnd();
+            return text;
+        }
+        public static string getData(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+          
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            string text = reader.ReadToEnd();
+            return text;
         }
     }
 }
